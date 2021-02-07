0859043f92cd7c55fbaeca8bb61e85f3665b741b
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2021-02-08 03:29:59 +0700

    simple query + mutation
    not completed

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,22 +1,45 @@
 const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+let links = [{
+  id: 'Link-0',
+  description: 'fullstack tutorial for graohql',
+  url: 'www.howtographql.com'
+}];
 
-const resolvers ={
-    Query:{
-      info: () => 'Test'
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => 'Test',
+    feed: () => links
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link
     }
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
+  }
 }
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
   resolvers
 });
 
-server.listen().then(({url})=>{
+server.listen().then(({ url }) => {
   console.log(`server is running on ${url}`);
 });
